617c16b7876e5b4aa2397299007c1c04dd8d5d06
Arvind <arvindcool2022@gmail.com>
2023-08-05 17:09:56 +0530

    In project one of working with json added a event.
    - clicking on the product card displays it on the container div.
    - yet to implement where it takes to new page.

--- working with json/project-1/main.js
+++ working with json/project-1/main.js
@@ -1,3 +1,4 @@
+//# PRODUCT PAGE CODE STARTS HERE
 const createProductsPage = (array) => {
     array.forEach(obj => {
         const element = `<div class="card">
@@ -6,30 +7,54 @@ const createProductsPage = (array) => {
                                 <p class="title">${obj.Title}</p>
                                 <p class="shortDescription">
                                     ${obj.ShortDescription}
-                                </p>
+                                    </p>
                                 <p class="price-detail">
                                     <span class="offerPrice">
-                                        <span class="currency">${obj.Currency}</span> 
-                                        ${obj.OfferPrice}
+                                    <span class="currency">${obj.Currency}</span> 
+                                    ${obj.OfferPrice}
                                     </span>
                                     <span class="originalprice">
                                         <span class="currency">${obj.Currency}</span>
                                         ${obj.OrignalPrice}
                                     </span>
                                     <span class="discountPercentage">${obj.DiscountPercentage}</span>
-                                </p>
-                                <p class="DeliveryDetails">${obj.DeliveryDetails}</p>
-                            </div>
-                        </div>`;
-                        document.querySelector('.wrapper').insertAdjacentHTML('beforeend', element);
+                                    </p>
+                                    <p class="DeliveryDetails">${obj.DeliveryDetails}</p>
+                                    </div>
+                                    </div>`;
+                                    document.querySelector('.wrapper').insertAdjacentHTML('beforeend', element);
     });
 }
 
 fetch('./product.json')
-    .then(jsonContent => jsonContent.json())
-    .then(convertedJson => productsPage(convertedJson));
+.then(jsonContent => jsonContent.json())
+.then(convertedJson => createProductsPage(convertedJson));
+
+/* 
+1. add event listener to wrapper that takes you to prduct show case page.
+2. add details of the the product to the html page through which target was clicked
+*/
+
+const wrapper = document.querySelector('.wrapper');
+
+const addProduct = (event) => {
+    let element = event.target
+    console.log(element)
+    let closestCard = element.closest(".card");
+    console.log(closestCard) //#The output in the console will show the div element itself.
+    let htmlContent = closestCard.outerHTML;
+    console.log(htmlContent) //#The output in the console will show the HTML markup as a string.
+    document.querySelector('.container').innerHTML = htmlContent;
+};
+  
+wrapper.addEventListener("click", addProduct);
 
 
+
+//# PRODUCT PAGE CODE ENDS HERE.
+
+
+//# CODE FOR TABLE STARTS HERE.
 const combineUniqueProperties = (arrayOfObj,array) => {
     for (const obj of arrayOfObj) {
         for (const key in obj) {
@@ -62,14 +87,16 @@ const createTable = (array) => {
             let content =`<td> ${obj[key]}</td>`
             let id =`row${obj.id}`
             document.getElementById(id).insertAdjacentHTML('beforeend', content);
-
+            
         }
     })
 } 
 
 
 fetch('./todo.json')
-    .then(response => response.json())
-    .then(mockData => createTable(mockData))
-    // .catch((error) => console.error('Error fetching data:', error));
+.then(response => response.json())
+.then(mockData => createTable(mockData))
+// .catch((error) => console.error('Error fetching data:', error));
 
+
+//# CODE FOR TABLE ENDS HERE.
