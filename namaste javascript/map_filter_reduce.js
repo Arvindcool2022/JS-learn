61e8f05febf85e5ed7b994a7365fc1b346235c6e
Arvind <arvindcool2022@gmail.com>
2023-08-12 07:27:54 +0530

    namaste JS Map, Filter and Reduce methods

---
+++ namaste javascript/map_filter_reduce.js
@@ -0,0 +1,41 @@
+const cricketPlayers = [
+  { firstName: 'sachin', lastName: 'tendulkar', age: 50 },
+  { firstName: 'rahul', lastName: 'dravid', age: 50 },
+  { firstName: 'virat', lastName: 'kolhi', age: 30 },
+  { firstName: 'ravi', lastName: 'sashthiri', age: 80 },
+  { firstName: 'virendra', lastName: 'sehwag', age: 52 },
+  { firstName: 'kapil', lastName: 'dave', age: 80 }
+];
+
+//* map()
+//# to transform an array and get a new array.
+//# if you want to create a newArray independent of orginalArray. Otherwise use forEach or ForOf to change(mutate) the originalArray
+
+const calculateArea = arr => arr.map(x => Math.PI * x * x);
+
+const fullName = cricketPlayers.map(x => x.firstName + ' ' + x.lastName);
+
+//* filter()
+//# to filter an array and get a new array (we a conditional to check and only if true the value will be returned to new array)
+
+const Greaterthan4 = arr => arr.filter(x => x > 4);
+
+const ageAbove50 = cricketPlayers.filter(x => x.age > 50);
+
+//* reduce()
+//#when we want a single value from an array.
+
+const sameAge = cricketPlayers.reduce((a, c) => {
+  a[c.age] ? ++a[c.age] : (a[c.age] = 1);
+  return a;
+}, {});
+
+const sameAge1 = cricketPlayers.reduce((acc, player) => {
+  if (acc[player.age]) ++acc[player.age];
+  else acc[player.age] = 1;
+
+  return acc;
+}, {});
+
+console.log(sameAge);
+console.log(sameAge1);
